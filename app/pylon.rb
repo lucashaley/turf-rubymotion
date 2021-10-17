341a1c1f1f75976bac3b20a5d9a63e671bee4c66
Lucas Haley <lucashaley@users.noreply.github.com>
2021-10-17 22:01:41 +1300

    Update pylon.rb

--- app/pylon.rb
+++ app/pylon.rb
@@ -1,14 +1,24 @@
 class Pylon
-  attr_reader :location, :owner, :lifespan, :birthdate
+  attr_reader :coordinate, :owner, :lifespan, :birthdate, :annotation
 
-  def initialize(location, owner, lifespan, birthdate)
-    @location = location
-    @owner = owner
-    @lifespan = lifespan
-    @birthdate = birthdate
+  def initialize(lat, long)
+    @coordinate = CLLocationCoordinate2D.new
+    @coordinate.latitude = lat
+    @coordinate.longitude = long
+    @annotation = MKPointAnnotation.alloc.initWithCoordinate(@coordinate)
+    # @owner = owner
+    # @lifespan = lifespan
+    # @birthdate = birthdate
   end
 
   def life
     return DateTime.now() - birthdate
   end
+
+  Test_Pylons = [
+    Pylon.new(37.33224775088951, -122.03043116202183),
+    Pylon.new(37.33241108181487, -122.03069094931799),
+    Pylon.new(37.33161364148488, -122.03045331411569),
+    Pylon.new(37.3327634456037, -122.02935601332467)
+  ]
 end
