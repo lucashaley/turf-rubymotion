07ef06603ed081dc6727a512c618ab3431a9006f
Lucas Haley <lucashaley@users.noreply.github.com>
2021-11-07 11:45:50 +1300

    Working through Firebase connections

--- app/game.rb
+++ app/game.rb
@@ -24,40 +24,33 @@ class Game
 
   def self.init_from_firebase(data)
     puts "\n\n----\ninit_from_firebase\ndata: #{data}\n\n"
+    _pylons = []
     # _this_ref = Machine.instance.db.child("games").orderByChild("gamecode").equalTo(data[:gamecode]).on("value")
     _games_ref = Machine.instance.db.referenceWithPath("games")
-    puts "_games_ref: #{_games_ref.URL}\n"
-    _games_ref.observeEventType(FIRDataEventTypeValue, withBlock:
-      lambda do |data|
-        puts "data: #{data}"
-        data.children.each do |c|
-          puts "\n#{c.value}"
-        end
-      end
-    )
-    puts data[:gamecode]
-    _this_ref = _games_ref.queryEqualToValue(data[:gamecode], childKey:"gamecode")
-    # Machine.instance.@db.referenceWithPath("games/#{data[:gamecode]}").observeEventType(FIRDataEventTypeValue, withBlock:Machine.instance.handleDataResult)
-    puts "_this_ref: #{_this_ref}"
-    _pylons = []
-    # _data = BW::JSON.parse(data.value)
-    # _this_ref.childSnapshotForPath("pylons").children.each do |p|
-    #   puts p.childSnapshotForPath("title").value
-    #   _pylons << Pylon.initWithLocation(\
-    #             CLLocationCoordinate2DMake( \
-    #             p.childSnapshotForPath("location/latitude").value, \
-    #             p.childSnapshotForPath("location/longitude").value), \
-    #             p.childSnapshotForPath("color").value, \
-    #             p.childSnapshotForPath("title").value)
-    # end
-    _games_ref.queryEqualToValue(data[:gamecode], childKey:"gamecode").observeSingleEventOfType(FIRDataEventTypeValue, withBlock:
+    _this_query = _games_ref.queryOrderedByChild("gamecode").queryEqualToValue(data[:gamecode])
+    puts "_this_query: #{_this_query}"
+    _this_query.observeSingleEventOfType(FIRDataEventTypeValue, withBlock:
       lambda do |snapshot|
-        puts "Snapshot: #{snapshot.hasChildren?}\n"
+        puts "Snapshot: #{snapshot.hasChildren}\n"
         snapshot.children.each do |child|
           puts "Child value: #{child.value}\n"
         end
       end
     )
+
+
+
+    _this_pylons = _this_query.ref.child("pylons")
+    puts "_this_pylons: #{_this_pylons.URL}"
+    _this_pylons.observeSingleEventOfType(FIRDataEventTypeValue, withBlock:
+      lambda do |snapshot|
+        puts "Snapshot: #{snapshot.hasChildren}\n"
+        snapshot.children.each do |child|
+          puts "Child value: #{child.value}\n"
+        end
+      end
+    )
+
     puts _pylons
   end
 
