9eca70ccc6231a75071978e3f4114c2b85d765fc
Lucas Haley <lucashaley@users.noreply.github.com>
2021-12-02 23:48:02 +1300

    Added team tables

--- app/new_controller.rb
+++ app/new_controller.rb
@@ -7,12 +7,40 @@ class NewController < UIViewController
   outlet :character_view, CharacterController
   outlet :cancel_button, UIButton
 
+  outlet :table_team_a, UITableView
+  outlet :table_team_b, UITableView
+  outlet :tableView, UITableView
+
+  outlet :not_close_enough, UILabel
+
+  DEBUGGING = true
+
   def viewDidLoad
     puts "NEWCONTROLLER: VIEWDIDLOAD".light_blue
+
+    super
+    table_team_a.delegate = self
+    # table_team_a.dataSource = something
+
     Machine.instance.current_view = self
 
     # get the current player's location
     Machine.instance.initialize_location_manager
+
+    # set player in db
+    @player_new_observer = App.notification_center.observe "PlayerNew" do |notification|
+      puts "PLAYER NEW".yellow
+
+      puts notification.object.value
+
+      handle_new_player
+    end
+
+    # @player_new_in_kapa_observer = App.notification_center.observe "PlayerNewInKapa" do |notification|
+    #   puts "PLAYER NEW IN KAPA".yellow
+    #   puts notification.object.value
+    #   handle_new_player_in_kapa
+    # end
 
     # create a new game in Firebase and retrieve its ID
     # TODO perhaps move this into viewWillAppear?
@@ -20,9 +48,43 @@ class NewController < UIViewController
     puts "New game uuID: #{Machine.instance.game.uuID.UUIDString}"
     gamecode.text = Machine.instance.game.gamecode
 
-    # set player in db
-
     Machine.instance.segue("ToCharacter")
+  end
+
+
+  ### TABLE STUFF
+  # Useful: https://code.tutsplus.com/tutorials/ios-sdk-crafting-custom-uitableview-cells--mobile-15702
+  def tableView(table_view, cellForRowAtIndexPath: index_path)
+    puts "NEWCONTROLLER TABLEVIEW CELLFORROW".blue if DEBUGGING
+
+    @reuseIdentifier ||= "PlayerCell"
+    cell = tableView.dequeueReusableCellWithIdentifier(@reuseIdentifier)
+
+    puts Machine.instance.game.nga_kapa[0].player_names[index_path.item]
+
+    # How can we tell which table it's from?
+    if table_view == @table_team_a
+      cell.player_name.text = Machine.instance.game.nga_kapa[0].player_names[index_path.item]
+    elsif table_view == @table_team_b
+      cell.player_name.text = "Georgina"
+    end
+
+    cell
+  end
+  def tableView(table_view, didDeselectRowAtIndexPath: index_path)
+    puts "NEWCONTROLLER TABLEVIEW DIDSELECT".blue if DEBUGGING
+  end
+  def tableView(table_view, numberOfRowsInSection: section)
+    puts "NEWCONTROLLER TABLEVIEW NUMBEROFROWS".blue if DEBUGGING
+    return 0 if Machine.instance.game.nga_kapa.empty? # This is a terrible concurrency hack
+    return Machine.instance.game.nga_kapa[0].count if table_view == @table_team_a
+    return Machine.instance.game.nga_kapa[1].count if table_view == @table_team_b
+  end
+  def handle_new_player
+    puts "NEWCONTROLLER HANDLE_NEW_PLAYER".blue if DEBUGGING
+    # TODO this is a hack
+    table_team_a.reloadData
+    table_team_b.reloadData
   end
 
   def cancel_new_game
@@ -51,4 +113,10 @@ class NewController < UIViewController
   def dismiss_new
     Machine.instance.segue("ToMenu")
   end
+end
+
+class PlayerCell < UITableViewCell
+  extend IB
+
+  outlet :player_name, UILabel
 end
