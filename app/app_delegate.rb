04a646cb053ac83e441b2eb67d03b41c06852e03
Lucas Haley <lucashaley@users.noreply.github.com>
2022-01-13 13:23:47 +1300

    Trying JavaScriptCore

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -8,6 +8,13 @@ class AppDelegate
 
     @window.makeKeyAndVisible
 
+    # Testing JavaScriptCore
+    puts "Testing JavaScriptCore"
+    js_context = JSContext.alloc.init
+    puts js_context
+    giga_value = js_context.evaluateScript("Math.pow(2,30)")
+    puts giga_value.toNumber
+
     # Start the StateMachine
     Machine.instance.fsm.start!
 
