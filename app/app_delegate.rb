99359b5fd4d20a2442a19b98309ef67c15f0e59d
Lucas Haley <lucashaley@users.noreply.github.com>
2022-01-18 11:29:41 +1300

    Trying JS Delaunator

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -12,8 +12,16 @@ class AppDelegate
     puts "Testing JavaScriptCore"
     js_context = JSContext.alloc.init
     puts js_context
-    giga_value = js_context.evaluateScript("Math.pow(2,30)")
-    puts giga_value.toNumber
+    # giga_value = js_context.evaluateScript("Math.pow(2,30)")
+    # puts giga_value.toNumber
+    js_file_path = NSBundle.mainBundle.pathForResource("d3-delaunay", ofType:"js")
+    js_url = NSBundle.mainBundle.URLForResource("d3-delaunay", withExtension:"js")
+    puts js_url.absoluteURL
+    error_ptr = Pointer.new(:object)
+    js_script = NSString.alloc.initWithContentsOfFile(js_file_path, encoding:NSUTF8StringEncoding, error:error_ptr)
+
+    js_context.evaluateScript(js_script, withSourceURL: js_url)
+    puts js_context["Delaunay"].constructWithArguments([[0, 0], [0, 1], [1, 0], [1, 1]])
 
     # Start the StateMachine
     Machine.instance.fsm.start!
