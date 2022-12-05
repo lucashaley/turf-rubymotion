019384112a473dc1bcda89cf91e34f5bcd1aaa4a
Lucas Haley <lucashaley@users.noreply.github.com>
2022-12-05 12:36:11 -0800

    Cleaning up app delegate

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -12,26 +12,26 @@ class AppDelegate
     @window.makeKeyAndVisible
 
     # Trying Logger
-    $logger = Motion::Lager.new(level: 'debug') # default
-    
+    # $logger = Motion::Lager.new(level: 'debug') # default
+
     # Trying BugSnag
     # Bugsnag.start
     # Bugsnag.notifyError(NSError.errorWithDomain('com.example'), code:408, userInfo:nil)
 
     # Testing JavaScriptCore
-    puts "Testing JavaScriptCore"
-    js_context = JSContext.alloc.init
-    puts js_context
-    # giga_value = js_context.evaluateScript("Math.pow(2,30)")
-    # puts giga_value.toNumber
-    js_file_path = NSBundle.mainBundle.pathForResource("d3-delaunay", ofType:"js")
-    js_url = NSBundle.mainBundle.URLForResource("d3-delaunay", withExtension:"js")
-    puts js_url.absoluteURL
-    error_ptr = Pointer.new(:object)
-    js_script = NSString.alloc.initWithContentsOfFile(js_file_path, encoding:NSUTF8StringEncoding, error:error_ptr)
-
-    js_context.evaluateScript(js_script, withSourceURL: js_url)
-    puts js_context["Delaunay"].constructWithArguments([[0, 0], [0, 1], [1, 0], [1, 1]])
+#     puts "Testing JavaScriptCore"
+#     js_context = JSContext.alloc.init
+#     puts js_context
+#     # giga_value = js_context.evaluateScript("Math.pow(2,30)")
+#     # puts giga_value.toNumber
+#     js_file_path = NSBundle.mainBundle.pathForResource("d3-delaunay", ofType:"js")
+#     js_url = NSBundle.mainBundle.URLForResource("d3-delaunay", withExtension:"js")
+#     puts js_url.absoluteURL
+#     error_ptr = Pointer.new(:object)
+#     js_script = NSString.alloc.initWithContentsOfFile(js_file_path, encoding:NSUTF8StringEncoding, error:error_ptr)
+#
+#     js_context.evaluateScript(js_script, withSourceURL: js_url)
+#     puts js_context["Delaunay"].constructWithArguments([[0, 0], [0, 1], [1, 0], [1, 1]])
 
     # Start the StateMachine
     Machine.instance.fsm.start!
