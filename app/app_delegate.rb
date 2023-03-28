4216dcc5f63e9e4923064cbff6f0e43890cc919d
Lucas Haley <lucashaley@users.noreply.github.com>
2023-03-28 12:23:41 -0700

    Clean up app_delegate.rb

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -2,19 +2,33 @@ class AppDelegate
   attr_accessor :window
 
   def application(application, didFinishLaunchingWithOptions: launchOptions)
-    puts "\n" * 16
-    puts 'STARTING APPLICATION'
-    puts "\n" * 16
+    debug_start_app
+    initialize_bugsnag
+    
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     storyboard = UIStoryboard.storyboardWithName("main", bundle: nil)
     @window.rootViewController = storyboard.instantiateInitialViewController
 
     @window.makeKeyAndVisible
 
-    # Trying Logger
-    # $logger = Motion::Lager.new(level: 'debug') # default
+    # Start the StateMachine
+    Machine.instance.fsm.start!
 
-    # Trying BugSnag
+    true
+  end
+  
+  def debug_start_app
+    # puts "\n" * 16
+    # puts 'STARTING APPLICATION'
+    # puts "\n" * 16
+    
+    mp "\n" * 8
+    mp 'Starting application'
+    mp "\n" * 8
+  end
+  
+  def initialize_bugsnag
+    # trying BugSnag
     mp 'Bugsnag...'
     begin
       Bugsnag.start
@@ -24,35 +38,5 @@ class AppDelegate
       mp exception.reason
       mp exception.callStackSymbols
     end
-
-    # Testing JavaScriptCore
-#     puts "Testing JavaScriptCore"
-#     js_context = JSContext.alloc.init
-#     puts js_context
-#     # giga_value = js_context.evaluateScript("Math.pow(2,30)")
-#     # puts giga_value.toNumber
-#     js_file_path = NSBundle.mainBundle.pathForResource("d3-delaunay", ofType:"js")
-#     js_url = NSBundle.mainBundle.URLForResource("d3-delaunay", withExtension:"js")
-#     puts js_url.absoluteURL
-#     error_ptr = Pointer.new(:object)
-#     js_script = NSString.alloc.initWithContentsOfFile(js_file_path, encoding:NSUTF8StringEncoding, error:error_ptr)
-#
-#     js_context.evaluateScript(js_script, withSourceURL: js_url)
-#     puts js_context["Delaunay"].constructWithArguments([[0, 0], [0, 1], [1, 0], [1, 1]])
-
-    # Start the StateMachine
-    Machine.instance.fsm.start!
-
-    true
   end
-
-  # # Stuff for FirebaseAuthUI
-  # def application(application, openURL: url, options: options)
-  #   puts 'APPDELEGATE application:openURL'.blue
-  #   return GIDSignIn.sharedInstance.handleUrl url
-  # end
-  #
-  # def authUI(authUI, didSignInWithAuthDataResult: result, error: error)
-  #   puts 'APPDELEGATE authUI:didSignIn'.blue
-  # end
 end
